480e7e006a9c589191e74abaad80b5d7aff69d79
njlyon0 <nicholasjlyon@gmail.com>
2025-09-11 15:42:39 -0400

    feat (setup): first pass at setup script to download data / make folders

---
+++ 00_setup.R
@@ -0,0 +1,73 @@
+## ---------------------------------------------------- ##
+# CFD - Step 1: Harmonization
+## ---------------------------------------------------- ##
+# Purpose:
+## Standarize and combine (i.e., "harmonize") input datasets
+
+# Load libraries
+librarian::shelf(tidyverse, googledrive)
+
+# Clear environment & collect garbage
+rm(list = ls()); gc()
+
+## --------------------------- ##
+# Make Folders ----
+## --------------------------- ##
+
+# Make the folders used by later scripts
+dir.create(path = file.path("data", "raw"), showWarnings = F, recursive = T)
+
+## --------------------------- ##
+# Download Data Key ----
+## --------------------------- ##
+
+# Identify the folder
+drive_folder <- googledrive::as_id("https://drive.google.com/drive/folders/1AxdFQ0EjNqaLUTzms4prF52cqbVFec0F")
+
+# Identify the relevant file(s) in that folder
+drive_key <- googledrive::drive_ls(path = drive_folder) %>% 
+  dplyr::filter(name == "CFD_Datakey")
+
+# Check that worked
+drive_key
+
+# Download it
+purrr::walk2(.x = drive_key$id, .y = drive_key$name,
+             .f = ~ googledrive::drive_download(file = .x, overwrite = T, type = "csv",
+                                                path = file.path("data", .y)))
+
+# Clear environment
+rm(list = ls()); gc()
+
+## --------------------------- ##
+# Download 'Raw' Data ----
+## --------------------------- ##
+
+# Identify the folder
+drive_folder <- googledrive::as_id("https://drive.google.com/drive/folders/1AxdFQ0EjNqaLUTzms4prF52cqbVFec0F")
+
+# Read in the data key
+key_df <- read.csv(file = file.path("data", "CFD_Datakey.csv"))
+
+# Check structure
+dplyr::glimpse(key_df)
+
+# What files are referenced in the key?
+sort(unique(key_df$source))
+
+# Identify the relevant file(s) in the Drive folder
+drive_raw <- googledrive::drive_ls(path = drive_folder) %>% 
+  dplyr::filter(name %in% key_df$source)
+
+# Check that worked
+drive_raw
+
+# Download it
+purrr::walk2(.x = drive_raw$id, .y = drive_raw$name,
+             .f = ~ googledrive::drive_download(file = .x, overwrite = T, 
+                                                path = file.path("data", "raw", .y)))
+
+# Clear environment
+rm(list = ls()); gc()
+
+# End ----
