494b73cf27af75c5fbf35fa3d0ef97c5d6119f41
mackwhite <mwhite@fiu.edu>
2025-09-15 10:20:39 -0400

    Exploring several of the new zooplankton datasets for the functional diversity WG meeting #1

---
+++ Data_exploration_scripts/mw-newdata-exploration.r
@@ -0,0 +1,16 @@
+###########################################################################
+# Script Information ------------------------------------------------------
+###########################################################################
+
+### project: LTER SPARC Consumer FXN Diversity
+### author(s): MW
+### goal: vizualizing some of the new datasets for Consumer WG
+
+# Housekeeping ------------------------------------------------------------
+
+### load necessary packages -----
+# install.packages("librarian")
+librarian::shelf(tidyverse, readxl, readr, ggpubr, corrplot, ggeffects)
+
+# read in necessary data and wrangle/summarize ----------------------------
+
