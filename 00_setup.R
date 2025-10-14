232d5b5aa3e525df42684a122a4e749831f9095d
njlyon0 <nicholasjlyon@gmail.com>
2025-10-14 13:14:22 -0400

    feat (setup): creating raw environmental data local folder

--- 00_setup.R
+++ 00_setup.R
@@ -22,7 +22,8 @@ purrr::walk(.x = c("00_keys",
                    "01_community_raw_data",
                    "02_community_processed_data",
                    "11_traits_raw_data",
-                   "12_traits_processed_data"),
+                   "12_traits_processed_data",
+                   "21_environmental_raw_data"),
             .f = ~ dir.create(path = file.path("data", .x),
                               showWarnings = F))
 
