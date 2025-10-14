b11c8c012dce703daf7450d5b5b37729849d9680
njlyon0 <nicholasjlyon@gmail.com>
2025-10-14 13:15:05 -0400

    refactor (shortcut): changing code syntax to be more human-readable and to allow users to choose which categories of data they want to (re-)download

--- 000_group-member-shortcut.R
+++ 000_group-member-shortcut.R
@@ -1,79 +1,82 @@
 ## ---------------------------------------------------- ##
-# CFD - Step 1: Harmonization
+# CFD - Group Member Shortcut
 ## ---------------------------------------------------- ##
 # Purpose:
-## Standarize and combine (i.e., "harmonize") input datasets
+## Download inputs required by the 'actual' workflow scripts
+## Requires access to this group's Shared Drive so this script is only run-able by current group members
+
+# Drive authentication note:
+## To run this code, you need to tell the `googledrive` R package who you are on Google
+## Work through this tutorial (est. 5 min) before trying to run this script:
+### https://lter.github.io/scicomp/tutorial_googledrive-pkg.html
 
 # Load libraries
-librarian::shelf(tidyverse, googledrive, fs)
+librarian::shelf(tidyverse, googledrive)
 
 # Clear environment & collect garbage
 rm(list = ls()); gc()
 
+# Create needed folders
+source("00_setup.R")
 
 ## --------------------------- ##
-# Make Folders ----
+# Download Keys ----
 ## --------------------------- ##
 
+# Grab relevant Drive folder URL
+key_drive <- googledrive::as_id("https://drive.google.com/drive/folders/1of2tKXcU_KnNDZrYLt3b3smObNrG_aXD")
 
-local_root <- "Data"  #local root folder
+# List items in that folder
+(key_files <- googledrive::drive_ls(path = key_drive))
 
-# List of folder names on Google Drive
-folders <- c(
-  "01_community_raw_data",
-   "02_community_processed_data",
-   "11_traits_raw_data",
-   "12_traits_processed_data",
-   "00_keys"
-)
+# Download them to the relevant local folder
+purrr::walk2(.x = key_files$id, .y = key_files$name,
+             .f = ~ googledrive::drive_download(file = .x, overwrite = T, type = "csv",
+                                                path = file.path("data", "00_keys", .y)))
 
- # Identify the folder
-parent <- googledrive::as_id("https://drive.google.com/drive/folders/1AxdFQ0EjNqaLUTzms4prF52cqbVFec0F")
- 
- # Get children folders under parent
-children <- drive_ls(parent, type = "folder")
-  
-# ---- FOR LOOP ----
-for (fld in folders) {
-  hit <- children[children$name == fld, ]
-  
-  if (nrow(hit) == 0) {
-    warning("Subfolder not found: ", fld)
-    next
-  }
-  
-  local_dir <- path(local_root, fld)
-  dir_create(local_dir, recurse = TRUE)
-  
-  message("\n== Downloading folder: ", fld, " ==")
-  
-  # List everything inside this subfolder (non-recursive)
-  items <- drive_ls(hit$id)
-  
-  if (nrow(items) == 0) {
-    message("  (empty)")
-    next
-  }
-  
-  for (i in seq_len(nrow(items))) {
-    it <- items[i, ]
-    name <- it$name
-    mime <- it$drive_resource[[1]]$mimeType
-    
-    # Build output filename
-    out_path <- path(local_dir, paste0(path_ext_remove(name), ".csv"))
-    
-    if (mime %in% c("application/vnd.google-apps.spreadsheet")) {
-      # Export Google-native files as CSV
-      message("⬇️  Exporting as CSV: ", out_path)
-      drive_download(as_id(it$id), path = out_path, type = "csv", overwrite = TRUE)
-    } else {
-      # For non-Google-native: just download as-is
-      message("⬇️  Downloading as-is: ", path(local_dir, name))
-      drive_download(as_id(it$id), path = path(local_dir, name), overwrite = TRUE)
-    }
-  }
-}
+## --------------------------- ##
+# Download Raw Community Data ----
+## --------------------------- ##
 
+# Grab relevant Drive folder URL
+comm_drive <- googledrive::as_id("https://drive.google.com/drive/folders/1n6iqs3aK2xWkROI8nPSVfk6ZkYPNpF7F")
 
-message("\n✅ Finished downloading all requested subfolders (Google-native exported to CSV).")
+# List items in that folder
+(comm_files <- googledrive::drive_ls(path = comm_drive))
+
+# Download them to the relevant local folder
+purrr::walk2(.x = comm_files$id, .y = comm_files$name,
+             .f = ~ googledrive::drive_download(file = .x, overwrite = T, type = "csv",
+                                                path = file.path("data", "01_community_raw_data", .y)))
+
+## --------------------------- ##
+# Download Raw Trait Data ----
+## --------------------------- ##
+
+# Grab relevant Drive folder URL
+trt_drive <- googledrive::as_id("https://drive.google.com/drive/folders/1UAq72kFD8Hh9uV1_He1ijJQ1m4lVg7eK")
+
+# List items in that folder
+(trt_files <- googledrive::drive_ls(path = trt_drive))
+
+# Download them to the relevant local folder
+purrr::walk2(.x = trt_files$id, .y = trt_files$name,
+             .f = ~ googledrive::drive_download(file = .x, overwrite = T, type = "csv",
+                                                path = file.path("data", "11_traits_raw_data", .y)))
+
+## --------------------------- ##
+# Download Environmental Data ----
+## --------------------------- ##
+
+# Grab relevant Drive folder URL
+env_drive <- googledrive::as_id("https://drive.google.com/drive/folders/1yUg4tYF7F-fuamODUOy55JUg8tmaUyYD")
+
+# List items in that folder
+(env_files <- googledrive::drive_ls(path = env_drive))
+
+# Download them to the relevant local folder
+purrr::walk2(.x = env_files$id, .y = env_files$name,
+             .f = ~ googledrive::drive_download(file = .x, overwrite = T, type = "csv",
+                                                path = file.path("data", "21_environmental_raw_data", .y)))
+
+# End ----
