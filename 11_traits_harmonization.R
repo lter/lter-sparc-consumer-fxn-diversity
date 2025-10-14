f9208a0d0384b4b63b2e691f65a84a81f59ca1f0
njlyon0 <nicholasjlyon@gmail.com>
2025-10-14 14:13:47 -0400

    feat (trait harmony): fleshed out key preparation and use

--- 11_traits_harmonization.R
+++ 11_traits_harmonization.R
@@ -4,5 +4,52 @@
 # Purpose:
 ## Combine separate trait tables into a single, "harmonized" traits table
 
+# Load libraries
+librarian::shelf(tidyverse, ltertools)
+
+# Get set up
+source("00_setup.R")
+
+# Clear environment & collect garbage
+rm(list = ls()); gc()
+
+## --------------------------- ##
+# Read in Keys ----
+## --------------------------- ##
+
+# Read in 'regular' trait key
+trt_key_v0 <- read.csv(file = file.path("data", "00_keys", "traits_datakey.csv"))
+
+# Make sure it's all pared down to the needed content
+trt_key <- ltertools::check_key(key = trt_key_v0)
+
+# Check structure
+dplyr::glimpse(trt_key)
+
+# Also read in the key for long-format traits
+trtlong_key_v0 <- read.csv(file = file.path("data", "00_keys", "long_traits_datakey.csv"))
+trtlong_key <- ltertools::check_key(key = trtlong_key_v0 )
+
+# Check structure
+dplyr::glimpse(trtlong_key)
+
+## --------------------------- ##
+# Use Data Keys ----
+## --------------------------- ##
+
+# Harmonize the 'regular' trait data
+trt_v1 <- ltertools::harmonize(key = trt_key, data_format = "csv",
+                               raw_folder = file.path("data", "11_traits_raw_data"))
+
+# Check structure
+dplyr::glimpse(trt_v1)
+
+# Do the same for the long-format traits
+trtlong_v1 <- ltertools::harmonize(key = trtlong_key, data_format = "csv",
+                               raw_folder = file.path("data", "11_traits_raw_data"))
+
+# Check structure
+dplyr::glimpse(trtlong_v1)
+
 
 # End ----
