8c16c502baf750fc69d46a28c7fe7bd35d28d53a
njlyon0 <nicholasjlyon@gmail.com>
2025-11-12 11:44:21 -0500

    refactor (spp): overhauled wrangling script for centralized filtering/wrangling

--- 22_species_wrangling.R
+++ 22_species_wrangling.R
@@ -4,7 +4,7 @@
 # Purpose:
 ## Data cleaning and taxa standardization for terrestrial species data
 
-#Load libraries
+# Load libraries
 librarian::shelf(tidyverse,  taxize)
 
 # Get set up
@@ -38,130 +38,57 @@ spp_v2 <- spp_v1 |>
                   .fns = ~ gsub(pattern = "\\\\ufffd", replacement = "", x = .))) |> 
   # Change all empty cells to true NAs
   dplyr::mutate(dplyr::across(.cols = dplyr::everything(),
-                              .fns = ~ ifelse(test = nchar(.) == 0, yes = NA, no = .)))
+                              .fns = ~ ifelse(test = nchar(.) == 0, yes = NA, no = .))) |> 
+  # Remove any rows where no taxonomic information is included
+  dplyr::filter(!is.na(scientific_name) | !is.na(order) | !is.na(family) | !is.na(genus) | !is.na(species) | !is.na(phylum) | !is.na(class) | !is.na(taxon_group) | !is.na(kingdom))
 
 # Check structure
 dplyr::glimpse(spp_v2)
-                              
 
 ## --------------------------- ##
 # Fix Scientific Name Info ----
 ## --------------------------- ##
 
 # Need to standardize how the scientific name is listed
+spp_v3 <- spp_v2 |> 
+  # Filter irreparable issues
+  ## Genera issues
+  dplyr::filter(!genus %in% c("sp.", paste0("unk", c(".", 1:3)))) |> 
+  dplyr::filter(stringr::str_detect(string = genus, pattern = "\\?") != T) |> 
+  ## Species issues
+  dplyr::filter(nchar(species) > 1 & !species %in% c("(HALIDAY)", "(LINNAEUS)", "(SAY)")) |> 
+  dplyr::filter(stringr::str_detect(string = species, pattern = "\\d") != T) |> 
+  dplyr::filter(stringr::str_detect(string = species, pattern = "\\?") != T) |> 
+  dplyr::filter(stringr::str_detect(string = species, pattern = "&") != T) |> 
+  # Repair what we can
+  dplyr::mutate(sci = dplyr::case_when(
+    is.na(scientific_name) & (!is.na(genus) & !is.na(species)) ~ paste0(genus, " ", species),
+    is.na(scientific_name) & (!is.na(genus) & is.na(species)) ~ paste0(genus, " sp."),
+    T ~ scientific_name))
 
+# Count NAs before/after
+supportR::count(vec = spp_v2$scientific_name) |> filter(is.na(value))
+supportR::count(vec = spp_v3$sci) |> filter(is.na(value))
 
+## --------------------------- ##
+# Tidy Rows/Columns ----
+## --------------------------- ##
 
+# Let's do some general cleaning
+spp_v4 <- spp_v3 |> 
+  # Reorder columns by taxonomic granularity
+  dplyr::relocate(kingdom, phylum, class, order, family, genus, species, sci, 
+    .after = source) |> 
+  # Remove superseded scientific name column
+  dplyr::select(-scientific_name) |> 
+  # Clarify actual species name column
+  dplyr::rename(scientific_name = sci, 
+    specific_epithet = species) |> 
+  # Keep only unique rows
+  dplyr::distinct()
 
-# BASEMENT ----
-
-species_list_v1 <- spp_v1
-
-run_taxa_check = "N" # "Y" indicate we need to run species check against ITIS "N" indicate we can skip the checking process
-
-
-### fill in scientific_name column for projects: HARVARD, PHOENIX, KONZA, SEV and SBC_BEACH
-
-
-########## HARVARD wrangling ###############
-
-HARVARD_v1 <- species_list_v1 %>%
-  dplyr::filter(project %in% "HARVARD") %>%
-  dplyr::mutate(scientific_name = paste(genus, species, sep = " ")) %>%
-  dplyr::relocate(c("kingdom", "phylum", "class"), .before = order) %>%
-  dplyr::distinct(scientific_name,.keep_all = TRUE)
-
-
-
-HARVARD_ready <- HARVARD_v1
-
-
-############# end ######################
-
-
-############# PHOENIX ###########################
-
-PHOENIX_v1 <- species_list_v1 %>%
-  dplyr::filter(project %in% "PHOENIX") %>%
-  dplyr::filter(!is.na(scientific_name)) %>%
-  dplyr::distinct(scientific_name,.keep_all = TRUE)
-
-PHOENIX_ready <- PHOENIX_v1
-
-
-########### KONZA wrangling ######################
-KONZA_v1 <- species_list_v1 %>%
-  dplyr::filter(project %in% "KONZA") %>%
-  dplyr::mutate(scientific_name = paste(genus, species, sep = " ")) %>%
-  dplyr::relocate(c("kingdom", "phylum", "class"), .before = order) 
-
-
-#remove unknown and unidentified species 
-
-KONZA_v2 <- KONZA_v1 %>%
-  dplyr::filter(!str_detect(scientific_name, "NA$")) %>% #remove species with suffix 'NA' 
-  dplyr::filter(!str_detect(scientific_name, "^unk")) %>%   #remove species with unknown scientific name - unk #
-  dplyr::filter(!grepl("\\d", scientific_name)) %>% #remove "sp."; "sp" and scientific_name with numbered species identifiers
-  dplyr::filter(!grepl("\\?", scientific_name)) %>%  #remove species with ? in name
-  dplyr::filter(sapply(strsplit(scientific_name, " "), length) <= 2) %>% #remove species with more than two names to include (), etc
-  dplyr::filter(!grepl("\\(|\\)", scientific_name)) %>%   #remove species with / in name
-  dplyr::filter(!str_detect(scientific_name, "^NA")) %>%     #remove species that begin with "NA_"
-  dplyr::distinct(scientific_name,.keep_all = TRUE) %>% # keep unique species names
-  dplyr::mutate(scientific_name = str_replace_all(scientific_name, pattern = "\\s+(sp\\.|spp\\.|sp|spp)$", replacement = ""))
-
-
-KONZA_ready <- KONZA_v2
-
-
-############ end ########################
-
-
-########### SEV #########################
-SEV_v1 <- species_list_v1 %>%
-  dplyr::filter(project %in% "SEV") %>%
-  dplyr::mutate(scientific_name = paste(genus, species, sep = " ")) %>%
-  dplyr::relocate(c("kingdom", "phylum", "class"), .before = order)%>%
-  dplyr::distinct(scientific_name,.keep_all = TRUE) %>%
-  dplyr::mutate(scientific_name = str_replace_all(scientific_name, pattern = "\\s+(sp\\.|spp\\.|sp|spp)$", replacement = ""))
-
-
-SEV_ready <- SEV_v1
-
-
-######## end ############################
-
-###### SBC #############################
-
-SBC_BEACH_v1 <- species_list_v1 %>%
-  dplyr::filter(project %in% "SBC_BEACH") %>%
-  dplyr::mutate(scientific_name = paste(genus, species, sep = " ")) %>%
-  dplyr::relocate(c("kingdom", "phylum", "class"), .before = order) %>%
-  dplyr::filter(class == "Aves") %>% #filter to remove all mammals and inanimate observations
-  dplyr::distinct(scientific_name,.keep_all = TRUE) %>%
-  dplyr::mutate(scientific_name = str_replace_all(scientific_name, pattern = "\\s+(sp\\.|spp\\.|sp|spp)$", replacement = "")) %>% # remove suffix sp.; sp; spp 
-  dplyr::filter(!str_detect(scientific_name, "\\d")) # remove rows with numbers -99999 na indicator 
-
-
-SBC_BEACH_ready <- SBC_BEACH_v1
-
-###### end ###############################
-
-
-
-#combine all sites back together once error in harmonization fixed  
-
-#sites_v00 <- species_list_v1 %>%
-  #dplyr::filter(project %in% c("HUBBARD", "MOHWAK"))
-
-sites_v00 <- species_list_v1 %>%
-  dplyr::filter(project %in% c("HUBBARD"))
-
-wrangled_sites_00 <- rbind(HARVARD_ready, PHOENIX_ready, KONZA_ready, SEV_ready, SBC_BEACH_ready)
-
-
-all_sites <- rbind(sites_v00, wrangled_sites_00)
-
-
+# Check structure
+dplyr::glimpse(spp_v4)
 
 ##########Only run this if we need to check the species against ITIS###############
 
