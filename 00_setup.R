e99318cc09b4d80f88c8cba0291e1a53bc240b6d
njlyon0 <nicholasjlyon@gmail.com>
2025-11-12 12:00:34 -0500

    feat (setup): adding a 'checks' subfolder of 'Data/'

--- 00_setup.R
+++ 00_setup.R
@@ -14,20 +14,22 @@ rm(list = ls()); gc()
 # Make Folders ----
 ## --------------------------- ##
 
-# Create folders
+# Create top-level folders
 dir.create(path = file.path("Data"), showWarnings = F)
 
-# Identify all data types
-data_types <- c("community", "traits", "environmental", "species")
+# Make some useful, one-off "Data/" subfolders
+dir.create(path = file.path("Data", "-keys"), showWarnings = F)
+dir.create(path = file.path("Data", "checks"), showWarnings = F)
 
 # Create 'raw' and 'tidy' subfolders of "Data/" for each data type
+## Identify all data types
+data_types <- c("community", "traits", "environmental", "species")
+## Actually make folders
 purrr::walk(.x = paste0(data_types, sort(rep(c("_raw-data", "_tidy-data"), 
                                                times = length(data_types)))),
             .f = ~ dir.create(path = file.path("Data", .x),
                               showWarnings = F, recursive = T))
 
-# Make a folder for the data keys as well
-dir.create(path = file.path("Data", "-keys"), showWarnings = F)
 
 # Clear environment & collect garbage
 rm(list = ls()); gc()
