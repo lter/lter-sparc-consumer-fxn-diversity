0778b83a267e9a021a4f3a3aad69b1a8ca815b46
njlyon0 <nicholasjlyon@gmail.com>
2025-11-12 10:11:53 -0500

    refactor (download): removing hard coding of "Data" folder in custom Drive download function and adding to argument

--- 000_group-member-shortcut.R
+++ 000_group-member-shortcut.R
@@ -19,31 +19,21 @@ source("00_setup.R")
 # Clear environment & collect garbage
 rm(list = ls()); gc()
 
-
 # a function to download the csv files from individual folder; 
 # could change the pattern if it is not only the csv files
-download_drive_folder <- function(folder_url, local_subfolder,pattern_in=NULL) {
+download_drive_folder <- function(folder_url, local_subfolder, pattern_in = NULL) {
   # Convert URL to Drive ID
   drive_id <- googledrive::as_id(folder_url)
   
   # List all files in the folder
-  files <- googledrive::drive_ls(path = drive_id,pattern=pattern_in)
+  files <- googledrive::drive_ls(path = drive_id, pattern = pattern_in)
   
   # Download each file to the specified local folder
-  purrr::walk2(
-    .x = files$id, 
-    .y = files$name,
-    .f = ~ googledrive::drive_download(
-      file = .x, 
-      overwrite = TRUE, 
-      type = "csv",
-      path = file.path("Data", local_subfolder, .y)
-    )
-  )
-  
-  # Clear environment and run garbage collection
-  rm(list = ls()); gc()
+  purrr::walk2(.x = files$id, .y = files$name,
+    .f = ~ googledrive::drive_download(file = .x, overwrite = TRUE, type = "csv",
+                                       path = file.path(local_subfolder, .y)))
 }
+
 ## --------------------------- ##
 # Download Keys ----
 ## --------------------------- ##
@@ -51,44 +41,43 @@ download_drive_folder <- function(folder_url, local_subfolder,pattern_in=NULL) {
 # Grab relevant Drive folder URL
 download_drive_folder(
   folder_url = "https://drive.google.com/drive/folders/1of2tKXcU_KnNDZrYLt3b3smObNrG_aXD",
-  local_subfolder = "-keys"
-)
+  local_subfolder = file.path("Data", "-keys"))
+
 ## --------------------------- ##
 # Download Raw Community Data ----
 ## --------------------------- ##
 
-# Grab relevant Drive folder URL
+# Download the contents of the relevant Drive folder to the respective local folder
 download_drive_folder(
   folder_url = "https://drive.google.com/drive/folders/1n6iqs3aK2xWkROI8nPSVfk6ZkYPNpF7F",
-  local_subfolder = "community_raw-data"
-)
-
+  local_subfolder = file.path("Data", "community_raw-data"))
 
 ## -------------------------- ##
 # Download Raw Terrestrial Species Data ----
 ## -------------------------- ##
 
+# Download the contents of the relevant Drive folder to the respective local folder
 download_drive_folder(
   folder_url = "https://drive.google.com/drive/folders/14H0hFBHxiqfubRBZ7DUUOSxMh5NOeiJN",
-  local_subfolder = "species_raw-data"
-)
+  local_subfolder = file.path("Data", "species_raw-data"))
                                                 
 ## --------------------------- ##
 # Download Raw Trait Data ----
 ## --------------------------- ##
 
+# Download the contents of the relevant Drive folder to the respective local folder
 download_drive_folder(
   folder_url = "https://drive.google.com/drive/folders/1UAq72kFD8Hh9uV1_He1ijJQ1m4lVg7eK",
-  local_subfolder = "traits_raw-data"
-)
+  local_subfolder = file.path("Data", "traits_raw-data"))
+
 ## --------------------------- ##
 # Download Environmental Data ----
 ## --------------------------- ##
 
+# Download the contents of the relevant Drive folder to the respective local folder
 download_drive_folder(
   folder_url = "https://drive.google.com/drive/folders/1yUg4tYF7F-fuamODUOy55JUg8tmaUyYD",
-  local_subfolder = "environmental_raw-data"
-)
+  local_subfolder = file.path("Data", "environmental_raw-data"))
 
 #################################################
 ##################################################
