a62245a294eedda8e0abf612a50f38317f86a7b3
njlyon0 <nicholasjlyon@gmail.com>
2025-11-12 14:01:46 -0500

    refactor (spp): added comments, streamlined operations, update filenames (in/out) for creation of 'master' list

--- 23_species_masterlist.R
+++ 23_species_masterlist.R
@@ -4,12 +4,8 @@
 # Purpose:
 ## Join species name from both aquatic and terrestrial sites 
 
-# Script author(s): Shalanda Grier, Li Kui, Nick Lyons
-
-
-#Load libraries
+# Load libraries
 librarian::shelf(tidyverse)
-
 
 # Get set up
 source("00_setup.R")
@@ -17,51 +13,60 @@ source("00_setup.R")
 # Clear environment & collect garbage
 rm(list = ls()); gc()
 
+## --------------------------- ##
+# Load Data ----
+## --------------------------- ##
 
-#Read in data 
-
-#excretion data 
+# Read in excretion data
 aqu_v1 <- read.csv(file.path("Data", "community_tidy-data", "04_harmonized_consumer_excretion_sparc_cnd_site.csv"))
 
-#glimpse(aqu_v1)
-
-aqu_v2 <- aqu_v1 %>%
+# Streamline this to just what we need here
+aqu_v2 <- aqu_v1 |> 
   dplyr::select(project, habitat, raw_filename, scientific_name, 
-                kingdom, phylum, class, order, family, genus)
+                kingdom, phylum, class, order, family, genus) |> 
+  dplyr::distinct()
 
-aqu_ready <- aqu_v2 %>% 
-  group_by(project) %>%
-  distinct(scientific_name, .keep_all = TRUE) #keep all distinct names for each project #check this again!
+# Check structure
+dplyr::glimpse(aqu_v2)
 
+# Terrestrial species names data 
+ter_v1 <- read.csv(file.path("Data", "species_tidy-data", "22_species_wrangled.csv"))
 
-#Terrestrial species names data 
-
-ter_v1 <- read.csv(file.path("Data", "species_tidy-data", "02_species_wrangled.csv"))
-
-ter_v2 <- ter_v1 %>%
-  dplyr::rename(raw_filename = source) %>%
+# Streamline this to just what we need here
+ter_v2 <- ter_v1 |> 
+  dplyr::rename(raw_filename = source) |> 
   dplyr::select(project, habitat, raw_filename, scientific_name, 
-                kingdom, phylum, class, order, family, genus)
-  
+                kingdom, phylum, class, order, family, genus) |> 
+  dplyr::distinct()
 
-ter_ready <- ter_v2
+# Check structure
+dplyr::glimpse(ter_v2)
 
+## --------------------------- ##
+# Combine Data ----
+## --------------------------- ##
 
-#combine aquatic and terrestrial consumer scientific_names to create a master list for all sites 
+# Combine the two species lists to get a single master list
+master_v1 <- dplyr::bind_rows(aqu_v2, ter_v2)
 
-master_list <- rbind(aqu_ready, ter_ready)
+# Check structure
+dplyr::glimpse(master_v1)
+
+## --------------------------- ##
+# Export ----
+## --------------------------- ##
 
 # Make one last version 
+master_v99 <- master_v1
 
-master_list_v99 <- master_list
-
+# Check structure
+dplyr::glimpse(master_v99)
 
 # Identify the file name & path
-master_list_file <- "03_species_masterlist.csv"
-master_list_path <- file.path("Data", "species_tidy-data", master_list_file)
+master_file <- "23_species_master-spp-list.csv"
+master_path <- file.path("Data", "species_tidy-data", master_file)
 
 # Export locally
-write.csv(x = master_list_v99, na = '', row.names = F, file = master_list_file)
-
+write.csv(x = master_v99, na = '', row.names = F, file = master_path)
 
 # End ----
