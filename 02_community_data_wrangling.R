69ac4804928aae5710db4e6118e2a03e3d3d93f9
lkuiucsb <lkui@ucsb.edu>
2025-12-08 11:50:51 -0800

    modify the codes to remove fishglob data

--- 02_community_data_wrangling.R
+++ 02_community_data_wrangling.R
@@ -1,5 +1,5 @@
 #----------------------------------##
-# SPARC - Consumer Functional Diversity (CFD) 
+# SPARC - Consumer Functional Diversity (CFD)
 ###--------------------------------##
 
 # Script author(s): Shalanda Grier, Li Kui, Nick Lyons
@@ -15,7 +15,7 @@
 # the taxa table has a species column, the same as scientific name; Right now, this code keep only the scientific name column; should we keep both columns?
 # We remove the phylum and family columns from the dry weight data since we will get that info from ITIS; is that ok? -okay and DONE
 # change the name from group to taxa_group so we don't have to change the code everywhere - DONE
-# what if there are more other taxa got added, how do we update the taxa table? So far it looks like a mannual process? 
+# what if there are more other taxa got added, how do we update the taxa table? So far it looks like a mannual process?
 # move the zoo_dry_wts data from the trait folder to the community folder. - DONE
 
 # Load libraries
@@ -34,14 +34,14 @@ rm(list = ls()); gc()
 run_species_check = "N" # "Y" indicate we need to run species check against ITIS "N" indicate we can skip the checking process
 
 ###  Wrangling zooplankton species names and pull kingdom, phylum, class, order, family, and species names from ITIS
-# read in zooplankton dry weight data 
+# read in zooplankton dry weight data
 zoo_dry_wts <- read.csv(file=file.path('Data', "community_raw-data", "Zooplankton_dry_ind_wt.csv"),na.strings=c("NA","NA ",""))
 
 
-#remove instances of 'sp.',  'spp.' , 'sp'  , 'spp. '; the scientific names and the "species" column is the same. 
+#remove instances of 'sp.',  'spp.' , 'sp'  , 'spp. '; the scientific names and the "species" column is the same.
 
-zoo_dry_wts_d1 <- zoo_dry_wts %>% 
-  dplyr::mutate(scientific_name = stringr::str_replace(scientific_name, " sp.$| spp.$| sp$| spp. $", ""), 
+zoo_dry_wts_d1 <- zoo_dry_wts %>%
+  dplyr::mutate(scientific_name = stringr::str_replace(scientific_name, " sp.$| spp.$| sp$| spp. $", ""),
 # Remove trailing spaces from 'text_column'
                 scientific_name = str_trim(scientific_name, side = "right")) %>%
   dplyr::select(-species_name_ori, -phylum,-family) %>%
@@ -60,7 +60,7 @@ zoo_dry_wts_d1 <- zoo_dry_wts %>%
 ##########Only run this if we need to check the species against ITIS###############
 
 if (run_species_check =="Y"){
-  
+
 zoo_taxa <- zoo_dry_wts_d1 %>%
  #select the scientific_name column. This column originally filled based on LTER sites reported species names
  dplyr::select(scientific_name)%>%
@@ -76,12 +76,12 @@ zoo_taxa <- zoo_dry_wts_d1 %>%
                species = NA)
 
 
-### add kingdom, phylum, order, family, genus and species name using taxize 
+### add kingdom, phylum, order, family, genus and species name using taxize
 
 for (i in 1:length(zoo_taxa$scientific_name)) {
-  
+
   sp <- zoo_taxa[i, ]$scientific_name
-  
+
   # Query species taxonomic information with error handling
   identified_species_names <- tryCatch(
     taxize::tax_name(
@@ -93,9 +93,9 @@ for (i in 1:length(zoo_taxa$scientific_name)) {
     ),
     error = function(e) NULL
   )
-  
+
   # Query species for common name with error handling; Note, ITIS does not have common names; IGNORE this step
-  
+
   # Fill taxonomy info safely
   zoo_taxa[i, ]$kingdom <- if (!is.null(identified_species_names)) paste0(identified_species_names$kingdom, collapse = "") else NA
   zoo_taxa[i, ]$phylum  <- if (!is.null(identified_species_names)) paste0(identified_species_names$phylum, collapse = "") else NA
@@ -104,7 +104,7 @@ for (i in 1:length(zoo_taxa$scientific_name)) {
   zoo_taxa[i, ]$family  <- if (!is.null(identified_species_names)) paste0(identified_species_names$family, collapse = "") else NA
   zoo_taxa[i, ]$genus   <- if (!is.null(identified_species_names)) paste0(identified_species_names$genus, collapse = "") else NA
   zoo_taxa[i, ]$species <- if (!is.null(identified_species_names)) paste0(identified_species_names$species, collapse = "") else NA
-  
+
 } #close the for loop
 
 # --- Error check at the end ---
@@ -116,9 +116,9 @@ if (length(unmatched) > 0) {
           paste(unmatched, collapse = ", "))
 } else {
   message("All species matched successfully!")
-} 
+}
 
-zoo_taxa_v2 <- zoo_taxa 
+zoo_taxa_v2 <- zoo_taxa
 #Replace the string "NA" with actual NA values
 zoo_taxa_v2[zoo_taxa_v2 == "NA"] <- NA
 
@@ -126,7 +126,7 @@ zoo_taxa_v2[zoo_taxa_v2 == "NA"] <- NA
 
 write.csv(x = zoo_taxa_v2, row.names = F, na = '',
           file = file.path("Data", "community_tidy-data","02_zoo_taxa_checked.csv"))
-} # close the species check 
+} # close the species check
 
 # if no need to check the taxa information, we can read the file directly
 zoo_taxa_checked<- read.csv(file = file.path("Data", "community_tidy-data", "02_zoo_taxa_checked.csv"),na="")
@@ -146,17 +146,17 @@ zoo_taxa_ready <- left_join(zoo_dry_wts_d1, zoo_taxa_checked, by = "scientific_n
      phylum = case_when(
        scientific_name == "Copepod nauplii" & is.na(phylum) ~ "Arthropoda",
        scientific_name == "Copepodites" & is.na(phylum) ~ "Arthropoda",
-       scientific_name == "Ctenophora" & is.na(phylum) ~ "Ctenophora", 
+       scientific_name == "Ctenophora" & is.na(phylum) ~ "Ctenophora",
        scientific_name == "Gammaridea" & is.na(phylum) ~ "Arthropoda",
-       scientific_name == "Spongiobranchia" & is.na(phylum) ~ "Mollusca", #WoRMS assignment 
-       TRUE ~ phylum # keep existing values for phylum column & remaining NA values 
+       scientific_name == "Spongiobranchia" & is.na(phylum) ~ "Mollusca", #WoRMS assignment
+       TRUE ~ phylum # keep existing values for phylum column & remaining NA values
      ),
      class = case_when(
        scientific_name == "Copepod nauplii" & is.na(class) ~ "Copepoda",
        scientific_name == "Copepodites" & is.na(class) ~ "Copepoda",
        scientific_name == "Gammaridea" & is.na(class) ~ "Malacostraca",
        scientific_name == "Spongiobranchia" & is.na(class) ~ "Gastropoda",
-       TRUE ~ class # keep existing values for class column & remaining NA values 
+       TRUE ~ class # keep existing values for class column & remaining NA values
      ),
      order = case_when(
        scientific_name == "Spongiobranchia" & is.na(order) ~ "Pteropoda",
@@ -168,10 +168,10 @@ zoo_taxa_ready <- left_join(zoo_dry_wts_d1, zoo_taxa_checked, by = "scientific_n
      ),
      genus = case_when(
        scientific_name == "Spongiobranchia" & is.na(genus) ~ "Spongiobranchia",
-       TRUE ~ genus #keep existing values for genus column & remaining NA values 
+       TRUE ~ genus #keep existing values for genus column & remaining NA values
      )
      )
-     
+
 ##################### end taxa cleaning ###############
 
 
@@ -179,55 +179,55 @@ zoo_taxa_ready <- left_join(zoo_dry_wts_d1, zoo_taxa_checked, by = "scientific_n
 
 
 #### community data ##############
-# read in harmonized data and start wrangling by project 
+# read in harmonized data and start wrangling by project
 
 com_dt<- read.csv(file = file.path("Data", "community_tidy-data", "01_community_harmonized.csv"))
 
 
-#### General Wrangling of Harmonized Community Data 
-## attend to capitalization 
+#### General Wrangling of Harmonized Community Data
+## attend to capitalization
 
-#change capitalization of species names to lower case 
+#change capitalization of species names to lower case
 com_dt2 <- com_dt %>%
   dplyr::mutate(species = tolower(species),
                 #remove trailing spaces from species column
                 species = str_trim(species, side = "right"),
-                #capitilize the first letter of each species 
+                #capitilize the first letter of each species
                 species = str_to_sentence(species),
-                #change date from character to date and separate year, month, and day 
+                #change date from character to date and separate year, month, and day
                 date = as.Date(date)
                 ) %>%
   dplyr::mutate_if(is.character, str_trim, side = "right")
 
 #### Arctic ###############
 
-Arctic  <-com_dt2 %>% 
+Arctic  <-com_dt2 %>%
   dplyr::filter(project == "Arctic")%>%
-  dplyr::select(-class) #drop class only need for RLS 
+  dplyr::select(-class) #drop class only need for RLS
 
 
-#convert density from num/l to num/m3 
+#convert density from num/l to num/m3
 
-Arctic_den <- Arctic %>% 
+Arctic_den <- Arctic %>%
   dplyr::mutate(`density_num/m3` = density*1000,
                 temp_c = "7.074284") %>%
-  mutate(sp_code = species) 
+  mutate(sp_code = species)
 
 Arctic_ready <- Arctic_den
 ########################### end #############################
 
-#########Palmer LTER 
+#########Palmer LTER
 
 Palmer <-com_dt2 %>%
   dplyr::filter(project == "Palmer") %>%
   dplyr::select(-class) #drop class
 
-#convert density from 1000m3 to 1m3. 
-#add temp data. SST retrieved from Palmer LTER weather station. 
-#Note average SST temp over duration of survey period 2009-2024. 
-#I believe every site had one temp value from CND or mean value for every year site combination? Not necessarily by depth. 
+#convert density from 1000m3 to 1m3.
+#add temp data. SST retrieved from Palmer LTER weather station.
+#Note average SST temp over duration of survey period 2009-2024.
+#I believe every site had one temp value from CND or mean value for every year site combination? Not necessarily by depth.
 
-Palmer_den <- Palmer %>% 
+Palmer_den <- Palmer %>%
   dplyr::mutate(`density_num/m3` = density/1000,
                  temp_c = "-0.4598606") %>%
   mutate(sp_code = species) %>%
@@ -237,149 +237,156 @@ Palmer_den <- Palmer %>%
 Palmer_ready <- Palmer_den
 ######### end ################
 
-###### North Lakes LTER 
+###### North Lakes LTER
 
-NorthLakes <- com_dt2 %>% 
+NorthLakes <- com_dt2 %>%
   dplyr::filter(project == "NorthLakes") %>%
   dplyr::select(-class) #drop class
 
-#convert density from m2 to m3. Divide density by tow_depth per EDI 
+#convert density from m2 to m3. Divide density by tow_depth per EDI
 #add temperature data for Southern Lakes  <https://lter.limnology.wisc.edu/core-study-lakes/>
-#note temp not by tow depths 
+#note temp not by tow depths
 
-NorthLakes_den <- NorthLakes %>% 
+NorthLakes_den <- NorthLakes %>%
   dplyr::mutate(`density_num/m3` = density/subsite_level2)%>%
   mutate(temp_c = case_when(site %in% 'FI' ~ 24.6,
                             site %in% 'WI' ~ 24.2,
                             site %in% 'MO' ~ 23.9,
-                            site %in% 'ME' ~ 23.2)) 
+                            site %in% 'ME' ~ 23.2))
  NorthLakes_ready <- NorthLakes_den
 
 ###################### North Lakes end  ##########################################
 
 #### Combine all sites (zooplankton) back together and add dry weight, diet_cat and scientific names
- 
+
 com_dt3 <- rbind(Arctic_ready, Palmer_ready, NorthLakes_ready) %>%
   dplyr::select(-c(density, kingdom,phylum, order, family, genus, species)) %>% #remove original density column
    #remove some duplicate column before joining, and use the taxa table information
-#add dry weight, diet_cat and scientific names and rename group column 
+#add dry weight, diet_cat and scientific names and rename group column
  dplyr::left_join(select(zoo_taxa_ready,-species), by= c("project","sp_code")) %>% # the "species" is the scientific name but from worms. #remove extra column names added after merge
  dplyr::rename_with(~ str_remove(., "\\..*")) %>%
  #dplyr::mutate(`density_num/m2` = NA) %>%
- dplyr::rename(`dmperind_g/ind` = drymass_g)
- 
+ dplyr::mutate(`dmperind_g/ind` = case_when(
+   `density_num/m3` == 0 ~ 0,
+   `density_num/m3`> 0 ~ drymass_g, #convert mg to g
+    TRUE ~ NA
+ )) %>%
+   dplyr::mutate(`dmperind_g/ind`=ifelse(is.na(`drymass_g`),NA,`dmperind_g/ind`)) %>%
+ dplyr::select(-drymass_g)
+
+
 com_dt4 <- com_dt3 #  [,-31] #remove boolean column
- 
+
 ##################################################
- 
- 
+
+
  ########################Reef Life Survey Australian Sites #############################
- 
+
  RLS <- com_dt %>%
    dplyr::filter(project == "RLS")
- 
+
  # call in dry weight conversions
  dm_conversion <- read.csv(file=file.path('Data', "community_raw-data", "dm_conversions_cndwg.csv"),na.strings=c("NA","NA ",""))
- 
- 
- #obtain individual biomass 
+
+
+ #obtain individual biomass
  #subsite_level2 = method
  #subsite_level3 = block
- #for each block for method 1 a total of 250m2 surveyed 
- #for each block for method 2 a total of 50m2 surveyed 
- #there are two blocks for each method 
- 
+ #for each block for method 1 a total of 250m2 surveyed
+ #for each block for method 2 a total of 50m2 surveyed
+ #there are two blocks for each method
+
  RLS_ind_bio <- RLS %>%
    dplyr::mutate(ind_bio = biomass_g/count_num) %>%
    dplyr::mutate(transectarea_m2 = case_when(
      subsite_level2 == 1 ~ 250,
      subsite_level2 == 2 ~ 50
    ))
- 
- 
- ##call in dry mass coeffeciet conversion data 
- 
+
+
+ ##call in dry mass coeffeciet conversion data
+
  dm_filter <- dm_conversion %>%
    dplyr::filter(kingdom == "Animalia"&dm_wm_mean < 1)
- 
- dm_coeff <- dm_filter %>% 
+
+ dm_coeff <- dm_filter %>%
    dplyr::group_by(class)%>%
-   dplyr::summarise(dm_coeff= mean(as.numeric(dm_wm_mean), 
+   dplyr::summarise(dm_coeff= mean(as.numeric(dm_wm_mean),
                                    na.rm=TRUE), .groups = "drop")%>%
    dplyr::ungroup()
- 
- #join dm_coeff with RLS survey data 
- 
+
+ #join dm_coeff with RLS survey data
+
  RLS_dm_coeff_data <- left_join(RLS_ind_bio, dm_coeff, by = "class")
- 
- 
+
+
  RLS_den_dm <- RLS_dm_coeff_data %>%
-   dplyr::mutate(`dmperind_g/ind` = ind_bio *dm_coeff, 
-                 `density_num/m2` = count_num/transectarea_m2, 
+   dplyr::mutate(`dmperind_g/ind` = ind_bio *dm_coeff,
+                 `density_num/m2` = count_num/transectarea_m2,
                  temp_c= case_when(
-                   site %in% "Jervis Bay" ~ 22.3,# assign temperature 
+                   site %in% "Jervis Bay" ~ 22.3,# assign temperature
                    site %in% "Maria Island" ~ 17,
                    site %in% "Ninepin Point" ~17
                  )
    ) %>%
    dplyr::select(-ind_bio)
- 
+
  #RLS_and_BottomTrawl_Fish_dietcat <- read_csv("~/Documents/RLS_and_BottomTrawl_Fish_dietcat.csv")
- #data not showing up in data folder on computer but when it does can replace above code with 
+ #data not showing up in data folder on computer but when it does can replace above code with
  RLS_and_BottomTrawl_Fish_dietcat <-read.csv(file=file.path('Data', "community_raw-data", "RLS_and_BottomTrawl_Fish_dietcat.csv"),na.strings=c("NA","NA ",""),strip.white = T)
- 
+
  diet_cat_1 <- RLS_and_BottomTrawl_Fish_dietcat[,-c(1,3:6)] %>%
    distinct()
- 
+
  RLS_den_dm2 <- RLS_den_dm %>%
   left_join(diet_cat_1, by = c("sp_code" = "taxon"))
- 
- 
-  RLS_ready <- RLS_den_dm2 %>%
-   dplyr::select(-transectarea_m2) 
-   
- 
- ############ end #################
- 
- 
- #############FISHGLOB Bottom trawl survey data North Sea and English Channel ######################  
- 
- FISHGLOB<- com_dt %>%
-   dplyr::filter(project == "FISHGLOB")
-  
- #dplyr::filter(!project %in% c("Arctic", "NorthLakes", "RLS", "Palmer")) # before fixing the issue with FISHGLOB data, metadata did not transfer over during harmonization
 
- 
- FISHGLOB_dm_coeff <- left_join(FISHGLOB, dm_coeff, by = "class")
- 
- FISHGLOB_ind_bio <- FISHGLOB_dm_coeff %>%
-   dplyr::mutate(biomass_g = biomass_kg*1000, #convert wet mass from kg to g
-                 ind_bio = biomass_g/count_num, #calculate biomass per individual 
-                 `dmperind_g/ind` = ind_bio * dm_coeff, #calculate dry mass per individual in g
-                 `density_num/m2` = `density_num_km2` /1000000, #convert density from km2 to m2 
-                 temp_c = case_when(
-                   site %in% "FR-CGFS" ~ 17, #add temp data for North Sea
-                   site %in% "NS-IBTS" ~ 16) #add temp data for English Channel 
-   )%>%
-   dplyr::select(-ind_bio)
- 
- 
- FISHGLOB_ind_bio1 <-  FISHGLOB_ind_bio %>%
-   left_join(diet_cat_1, by = c("species" = "taxon"))
- 
- FISHGLOB_ready <-FISHGLOB_ind_bio1
-   
+
+  RLS_ready <- RLS_den_dm2 %>%
+   dplyr::select(-transectarea_m2)
+
+
+ ############ end #################
+
+
+ #############FISHGLOB Bottom trawl survey data North Sea and English Channel ######################
+ # # we decide to remove this fighglob dataset for now.
+ # FISHGLOB<- com_dt %>%
+ #   dplyr::filter(project == "FISHGLOB")
+ #
+ # #dplyr::filter(!project %in% c("Arctic", "NorthLakes", "RLS", "Palmer")) # before fixing the issue with FISHGLOB data, metadata did not transfer over during harmonization
+ #
+ #
+ # FISHGLOB_dm_coeff <- left_join(FISHGLOB, dm_coeff, by = "class")
+ #
+ # FISHGLOB_ind_bio <- FISHGLOB_dm_coeff %>%
+ #   dplyr::mutate(biomass_g = biomass_kg*1000, #convert wet mass from kg to g
+ #                 ind_bio = biomass_g/count_num, #calculate biomass per individual
+ #                 `dmperind_g/ind` = ind_bio * dm_coeff, #calculate dry mass per individual in g
+ #                 `density_num/m2` = `density_num_km2` /1000000, #convert density from km2 to m2
+ #                 temp_c = case_when(
+ #                   site %in% "FR-CGFS" ~ 17, #add temp data for North Sea
+ #                   site %in% "NS-IBTS" ~ 16) #add temp data for English Channel
+ #   )%>%
+ #   dplyr::select(-ind_bio)
+ #
+ #
+ # FISHGLOB_ind_bio1 <-  FISHGLOB_ind_bio %>%
+ #   left_join(diet_cat_1, by = c("species" = "taxon"))
+ #
+ # FISHGLOB_ready <-FISHGLOB_ind_bio1
+
 ##############  end #############
- 
- 
- #combine fish data and add diet_cat information 
- 
- fish_com_dt1 <- rbind(RLS_ready, FISHGLOB_ready) %>%
+
+
+ #combine fish data and add diet_cat information
+
+ fish_com_dt1 <- RLS_ready %>%
    dplyr::select(-c(density))
- 
- 
-###combine consumer data 
-#check differences in column names and check column names to prepare to combine all consumer data 
+
+
+###combine consumer data
+#check differences in column names and check column names to prepare to combine all consumer data
 #setdiff(names(com_dt4), names(fish_com_dt2))
 
 
@@ -390,37 +397,37 @@ fish_com_ready <- fish_com_dt1 %>%
   dplyr::mutate(taxonomicLevel = NA) %>%
   dplyr::mutate(subsite_level3= NA) %>%
   dplyr::select(-c(dm_coeff))
- 
-zoo_com_ready<- com_dt4 %>% #add new column names so each dataframes matches. can delete later 
+
+zoo_com_ready<- com_dt4 %>% #add new column names so each dataframes matches. can delete later
   dplyr::mutate(length_cm = NA,
                 biomass_kg = NA,
                 taxonomicLevel = NA,
                 `density_num/m2` = NA)
-  
+
 #sort(colnames(zoo_com_ready))
 #sort(colnames(fish_com_ready))
- 
+
 
 all_com_data <- rbind(zoo_com_ready, fish_com_ready)
- 
- 
+
+
  ## --------------------------- ##
  # Export ----
  ## --------------------------- ##
- 
+
  # Make one last version of the data
  com_v99 <-all_com_data
- 
+
  # Check structure
  dplyr::glimpse(com_v99)
- 
+
  # Identify the file name & path
  comm_file <- "02_community_wrangled.csv"
  comm_path <- file.path("Data", "community_tidy-data", comm_file)
- 
+
  # Export locally
  write.csv(x =  com_v99, na = '', row.names = F, file = comm_path)
- 
+
  # End ----
 
- 
+
--- 03_community_excretion_rate.R
+++ 03_community_excretion_rate.R
@@ -1,5 +1,5 @@
 #----------------------------------##
-# SPARC - Consumer Functional Diversity (CFD) 
+# SPARC - Consumer Functional Diversity (CFD)
 ###--------------------------------##
 
 # Script author(s): Shalanda Grier, Li Kui, Nick Lyons
@@ -16,17 +16,17 @@ rm(list = ls()); gc()
 
 
 # read in the harmonized data
-df <- read.csv(file.path("Data", "community_tidy-data","02_community_wrangled.csv"),stringsAsFactors = F,na.strings =c("")) 
+df <- read.csv(file.path("Data", "community_tidy-data","02_community_wrangled.csv"),stringsAsFactors = F,na.strings =c(""))
 
 #remove the columns that are not needed
 dfrm <- df %>%
   select(-c(density_num_km2,biomass_kg,taxonomicLevel))
 #### calculate excretion rate
 
-# take out the rows that are needed 
+# take out the rows that are needed
 
 df1 <-dfrm %>%
-  rename(dmperind=`dmperind_g.ind`,temp=temp_c) 
+  rename(dmperind=`dmperind_g.ind`,temp=temp_c)
 
 peace<- df1 %>%
   filter(is.na(dmperind)) %>%
@@ -37,8 +37,8 @@ peace2<-df1 %>%
   filter(is.na(diet_cat)) %>%
   distinct(project,habitat,sp_code,scientific_name,diet_cat)
 
-peace3 <-df1 |> 
-  filter(is.na(scientific_name)) 
+peace3 <-df1 |>
+  filter(is.na(scientific_name))
 
 # remove all the species that should not be in the data
 df2<-df1 %>%
@@ -63,13 +63,13 @@ df2<-df1 %>%
 #using bradley's code below for excretion calculation
 cons <- df2
 
-cons_np_ratio <- cons %>% 
+cons_np_ratio <- cons %>%
   #N
   mutate(N_vert_coef = if_else(phylum == "Chordata", 0.7804, 0),
          N_diet_coef = if_else(diet_cat == "algae_detritus", -0.0389,
                                if_else(diet_cat == "invert", -0.2013,
                                        if_else(diet_cat == "fish", -0.0537,
-                                               if_else(diet_cat == "fish_invert", -0.1732, 
+                                               if_else(diet_cat == "fish_invert", -0.1732,
                                                        if_else(diet_cat == "algae_invert", 0,
                                                                NA))))),
          Nexc_log10 = ifelse(`dmperind` > 0, 1.461 + 0.6840*(log10(`dmperind`)) + 0.0246*temp + N_diet_coef + N_vert_coef,NA),
@@ -80,12 +80,12 @@ cons_np_ratio <- cons %>%
          P_diet_coef = if_else(diet_cat == "algae_detritus", 0.0173,
                                if_else(diet_cat == "invert", -0.2480,
                                        if_else(diet_cat == "fish", -0.0337,
-                                               if_else(diet_cat == "fish_invert", -0.4525, 
+                                               if_else(diet_cat == "fish_invert", -0.4525,
                                                        if_else(diet_cat == "algae_invert",0,
                                                                NA))))),
          Pexc_log10 = ifelse(`dmperind` >0, 0.6757 + 0.5656*(log10(`dmperind`)) + 0.0194*temp + P_diet_coef + P_vert_coef, NA),
          `pind_ug/hr` = 10^Pexc_log10,
-         `pind_ug/hr` = ifelse(is.na(`pind_ug/hr`),0,`pind_ug/hr`))  
+         `pind_ug/hr` = ifelse(is.na(`pind_ug/hr`),0,`pind_ug/hr`))
 
 
 ##################### end of bradley's code #####################
@@ -103,17 +103,17 @@ exc_df <- cons_np_ratio |>
 cnd <- read.csv(file.path("Data", "community_raw-data","harmonized_consumer_excretion_CLEAN.csv"),stringsAsFactors = F,na.strings =c("."))
 
 # colnames(cnd)
-# #[1] "project"         "habitat"         "raw_filename"    "row_num"         "year"            "month"           "day"             "date"           
-# #[9] "site"            "subsite_level1"  "subsite_level2"  "subsite_level3"  "sp_code"         "scientific_name" "species"         "density_num.m"  
-# #[17] "dmperind_g.ind"  "temp_c"          "density_num.m2"  "density_num.m3"  "common_name"     "kingdom"         "phylum"          "class"          
-# #[25] "order"           "family"          "genus"           "taxa_group"      "diet_cat"        "nind_ug.hr"      "pind_ug.hr" 
+# #[1] "project"         "habitat"         "raw_filename"    "row_num"         "year"            "month"           "day"             "date"
+# #[9] "site"            "subsite_level1"  "subsite_level2"  "subsite_level3"  "sp_code"         "scientific_name" "species"         "density_num.m"
+# #[17] "dmperind_g.ind"  "temp_c"          "density_num.m2"  "density_num.m3"  "common_name"     "kingdom"         "phylum"          "class"
+# #[25] "order"           "family"          "genus"           "taxa_group"      "diet_cat"        "nind_ug.hr"      "pind_ug.hr"
 
 cnd1<-cnd %>%
   dplyr::select(-row_num,-density_num.m,-common_name,-species) %>%
-  mutate(length_cm=NA,count_num=NA, biomass_g=NA) 
+  mutate(length_cm=NA,count_num=NA, biomass_g=NA)
 
-###combine consumer data 
-#check differences in column names and check column names to prepare to combine all consumer data 
+###combine consumer data
+#check differences in column names and check column names to prepare to combine all consumer data
 #setdiff(names(exc_df),names(cnd1))
 comball <- rbind(exc_df, cnd1)
 
@@ -132,6 +132,8 @@ tidy_path1 <- file.path("Data", "community_tidy-data", tidy_filename1)
 # Export locally
 write.csv(x = comball, na = '', row.names = F, file = tidy_path1)
 
+###open the file
+cfd <- read.csv(file.path("Data", "community_tidy-data","03_harmonized_consumer_excretion_sparcsite.csv"),stringsAsFactors = F,na.strings =c(""))
 
 
 # End ----
