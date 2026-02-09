77f03d17bab2e7aefd59f335a1e8ef3a7d61f2b0
mackwhite <mwhite@fiu.edu>
2026-02-09 11:13:29 -0800

    creating missing trait dataset

--- Data_exploration_scripts/mw_bird_species_list_goodies.R
+++ Data_exploration_scripts/mw_bird_species_list_goodies.R
@@ -23,3 +23,27 @@ glimpse(dat)
 nacheck(dat)
 unique(dat$project)
 write_csv(dat, '../Collaborative/FnxSynthBase/bird_species_list.csv')
+
+dat1 <- read_csv('../Collaborative/FnxSynthBase/12_traits_wrangled.csv')
+
+dat2 <- dat1 |> select(source, family, scientific_name, genus, epithet, 
+                       taxonomic.resolution, taxon, mass_adult_g, length_adult_cm, age_life.span_years,
+                       reproduction_reproductive.rate_num.events.per.year, reproduction_reproductive.rate_num.litter.or.clutch.per.year,
+                       diet_trophic.level_num, active.time_category_ordinal)
+
+dat3 <- dat |> left_join(dat2)
+glimpse(dat3)
+
+dat4 <- dat3 |> 
+      select(kingdom, phylum, class, order, family, genus, epithet, scientific_name,
+             mass_adult_g, length_adult_cm, age_life.span_years,
+             reproduction_reproductive.rate_num.events.per.year, reproduction_reproductive.rate_num.litter.or.clutch.per.year,
+             diet_trophic.level_num, active.time_category_ordinal) |> 
+      distinct() |> 
+      group_by(kingdom, phylum, class, order, family, genus, epithet, scientific_name) |> 
+      mutate(across(where(is.numeric), ~(mean(.x, na.m = TRUE)))) |> 
+      ungroup() |> 
+      distinct()
+glimpse(dat4)
+
+write_csv(dat4, '../Collaborative/FnxSynthBase/bird_species_list_withtraitsmissing.csv')
